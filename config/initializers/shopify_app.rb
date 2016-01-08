00728f919f4da4e288e5311d616d5e537b85ccaf
Carryall <carryall369@gmail.com>
2016-01-08 14:35:25 +0700

    add route and controllers

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,12 @@
 ShopifyApp.configure do |config|
-  config.api_key = "20cf41bda0442582ae15cc20f65bd179"
-  config.secret = "42d6850ef899ba1b8988c975005c1b6d"
-  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
+  # private
+  # config.api_key = "20cf41bda0442582ae15cc20f65bd179"
+  # config.secret = "42d6850ef899ba1b8988c975005c1b6d"
+
+  # public
+  config.api_key = "ebc9735e859e204a1b7df25c0f2756c9"
+  config.secret = "b6fb6c3fa5f8667b5c82d66e92a9dc54"
+  config.redirect_uri = "http://test-api-limit.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
+  controller :sessions do
+    get 'login' => :new, :as => :login
+    post 'login' => :create, :as => :authenticate
+    get 'auth/shopify/callback' => :callback
+    get 'logout' => :destroy, :as => :logout
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
