9bdbd03d5368e2996f1171bdbccec873fde92410
Carryall <carryall369@gmail.com>
2016-01-11 11:59:39 +0700

    render layout

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,21 +1,21 @@
 class ProxyController < ApplicationController
 
   def index
-    shop = Shop.where(shopify_domain: params[:shop]).first
+    # shop = Shop.where(shopify_domain: params[:shop]).first
 
-    session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
-    ShopifyAPI::Base.activate_session(session)
+    # session = ShopifyAPI::Session.new(shop.shopify_domain, shop.shopify_token)
+    # ShopifyAPI::Base.activate_session(session)
 
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+    # @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
     # @products = []
     render :layout => false, :content_type => 'application/liquid'
   end
 
-  def render_text
-    render text: 'Hello World' and return
-  end
+  # def render_text
+  #   render text: 'Hello World' and return
+  # end
 
-  def render_text_liquid
-    render text: 'Hello World Liquid', :layout => false, :content_type => 'application/liquid'
-  end
+  # def render_text_liquid
+  #   render text: 'Hello World Liquid', :layout => false, :content_type => 'application/liquid'
+  # end
 end
